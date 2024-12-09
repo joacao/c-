9a1759a65721b36fab6f4074f5b1d0a2e4f0b5ee
joacao <henriquejoaquim.hd@gmail.com>
2024-12-09 06:03:38 -0300

    Adicionando arquivos primeiros arquivos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Screen Sound
+string mensagemBoasVindas = "Boas vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+    
+█▀ █▀▀ █▀█ █▀▀ █▀▀ █▄░█   █▀ █▀█ █░█ █▄░█ █▀▄
+▄█ █▄▄ █▀▄ ██▄ ██▄ █░▀█   ▄█ █▄█ █▄█ █░▀█ █▄▀
+");
+    Console.WriteLine(mensagemBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
